450ed7bece782b31bf33f3a57834bf575ffebf88
JSRosu <88613736+JSRosu@users.noreply.github.com>
2021-08-10 15:41:36 +0530

    test

---
+++ LearnIfElse.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 3082;
+		
+		if (number == 0) {
+			
+			System.out.println("The number is neutral.");
+		}
+		else if (number%2 != 0) {
+			System.out.println("The number is odd.");
+				
+		}
+		else
+			System.out.println("The number is even");
+	}
+
+}
