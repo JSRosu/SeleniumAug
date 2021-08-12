f63bcccbae70d0f4553a7111e94ca3e73dbbcff7
JSRosu <88613736+JSRosu@users.noreply.github.com>
2021-08-12 09:59:01 +0530

    Add files via upload

---
+++ MyKid.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class MyKid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MyMobile kidObj = new MyMobile();
+		
+		float amps = kidObj.amps; // assignig to a variable ctrl+1
+		System.out.println("The value of amps is : " + amps);
+		kidObj.makeCalls();
+		kidObj.takePics();
+		
+		
+		
+		
+	}
+
+}
